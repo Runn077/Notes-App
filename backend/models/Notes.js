4c1392aba24426a8f908d681182ea33b862c1ba2
Runn077 <rupu0777@gmail.com>
2025-10-12 22:25:34 -0600

    created Notes model

---
+++ backend/models/Notes.js
@@ -0,0 +1,13 @@
+module.exports=(sequelize, DataTypes) => {
+    const Notes = sequelize.define("Notes", {
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        postBody: {
+            type: DataTypes.STRING,
+            allowNull: true
+        }
+    });
+    return Notes;
+}
