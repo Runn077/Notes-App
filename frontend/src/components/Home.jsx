be621b4166dfd68269c25e2f62ea1ee6705b3efc
Runn077 <rupu0777@gmail.com>
2025-10-16 09:20:01 -0600

    Added notes list

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -5,15 +5,28 @@ function Home() {
   const [listOfNotes, setListOfNotes] = useState([]);
 
   useEffect(() => {
-    axios.get(`http://localhost:3001/notes`).then((response) => {
-        settListOfNotes(response.data)
+    axios.get(`http://localhost:3001/notes`)
+    .then((response) => {
+        setListOfNotes(response.data)
+        console.log(response.data)
+    })
+    .catch((error) => {
+        console.log("Error fetching data: " + error)
     });
-  });
+  }, []);
+
   return (
-    <div>
-      Home Page
+    <div className="homeContainer">
+        {listOfNotes.map((note, index) => {
+            return (
+            <div className = "note">
+                <div className='title'>{note.title}</div>
+                <div className='postBody'>{note.postBody}</div>
+            </div>
+            );
+        })}  
     </div>
-  )
-}
+  );
+};
 
 export default Home
