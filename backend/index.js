2fba31d47f8c50a65f0afab2ce5fa70cc7e46fcb
Runn077 <rupu0777@gmail.com>
2025-10-16 09:18:26 -0600

    added cors

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,10 @@
 require('dotenv').config();
-
-const express = require('express')
+const express = require('express');
 const app = express();
-const db = require("./models")
+const db = require("./models");
+const cors = require("cors");
 
+app.use(cors())
 app.use(express.json());
 
 const notesRouter = require("./routes/Notes");
