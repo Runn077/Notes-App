2f06c612fdabf50bb073e05e50c064ad77ff9473
Runn077 <rupu0777@gmail.com>
2025-10-16 11:08:42 -0600

    Allow postBody to be longer

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 const db = require("./models");
 const cors = require("cors");
+const sequelize = require('./models').sequelize;
 
 app.use(cors())
 app.use(express.json());
@@ -11,7 +12,7 @@ const notesRouter = require("./routes/Notes");
 app.use("/notes", notesRouter);
 
 const port = process.env.PORT
-db.sequelize.sync().then(() => {
+db.sequelize.sync({ alter: true }).then(() => {
     app.listen(port, () => {
         console.log("Server Running on Port: " + port);
     });
