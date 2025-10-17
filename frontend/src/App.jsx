1bbafbf1d9d473057efa8be2bdf158ca050c4923
Runn077 <rupu0777@gmail.com>
2025-10-17 11:38:27 -0600

    deleted CreateNotes page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,11 @@
 import './App.css'
 import Home from './components/Home'
-import CreateNote from './components/CreateNotes'
 import { createBrowserRouter, RouterProvider } from "react-router-dom"
 
 const router = createBrowserRouter([
   {path:"/", element: <Home />},
-  {path:"/createnote", element: <CreateNote />}
 ])
+
 function App() {
   return (
     <RouterProvider router={router} />
