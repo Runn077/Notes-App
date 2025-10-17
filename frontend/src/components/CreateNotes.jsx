a183ab1ea54a792c3db01c8536d33d9c3e6af317
Runn077 <rupu0777@gmail.com>
2025-10-17 02:34:29 -0600

    Created a create note page

---
+++ frontend/src/components/CreateNotes.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function CreateNote() {
+  return (
+    <div className="createNewNote">
+        <textarea placeholder="Enter Title" className="title">
+        </textarea>
+    </div>
+  )
+}
+
+export default CreateNote
