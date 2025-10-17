3012fea3964b27077d37ee6933076150a4e0cc09
Runn077 <rupu0777@gmail.com>
2025-10-17 02:34:58 -0600

    added reacter router dom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,16 @@
 import './App.css'
 import Home from './components/Home'
+import CreateNote from './components/CreateNotes'
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
 
+const router = createBrowserRouter([
+  {path:"/", element: <Home />},
+  {path:"/createnote", element: <CreateNote />}
+])
 function App() {
-
   return (
-    <>
-    <Home />
-    </>
-  )
+    <RouterProvider router={router} />
+  );
 }
 
 export default App
