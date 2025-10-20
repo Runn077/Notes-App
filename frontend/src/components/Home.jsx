08af29a2b68880e1146220c15855db072f202b4a
Runn077 <rupu0777@gmail.com>
2025-10-19 23:18:08 -0600

    added an input to mave the save functional

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -4,6 +4,7 @@ import axios from 'axios';
 function Home() {
   const [listOfNotes, setListOfNotes] = useState([]);
   const [modal, setModal] = useState(false);
+  const [newTitle, setNewTitle] = useState('');
 
   useEffect(() => {
     axios.get(`http://localhost:3001/notes`)
@@ -19,6 +20,15 @@ function Home() {
   const handleClick = () =>{
     setModal(!modal)
   }
+
+  const handleSave = () => [
+    axios.post(`http://localhost:3001/notes`, {title: newTitle, postBody: ''})
+      .then((response) => {
+        setNewTitle('');
+        setModal(false);
+        setListOfNotes([...listOfNotes, response.data]);
+      })
+  ]
 
   return (
     <div className="homeContainer">
@@ -36,6 +46,13 @@ function Home() {
               <div className='overlay'></div>
               <div className='modalContent'>
                 <div className='modalTitle'>Create New Note</div>
+                <input 
+                  placeholder='Title Name' 
+                  value={newTitle} 
+                  className='titleInput'
+                  onChange = {(e)=> setNewTitle(e.target.value)}>
+                </input>
+                <button onClick={handleSave} className='save'>Save</button>
                 <button onClick={handleClick} className='closeModalButton'>Close</button>
               </div>
             </div>
