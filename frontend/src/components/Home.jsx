b604bed83a27a9a486256fb2d10ee5d4f408d7cc
Runn077 <rupu0777@gmail.com>
2025-10-20 10:41:10 -0600

    put the create button on top of the home page

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -5,6 +5,7 @@ function Home() {
   const [listOfNotes, setListOfNotes] = useState([]);
   const [modal, setModal] = useState(false);
   const [newTitle, setNewTitle] = useState('');
+  const [titleErrMsg, setTitleErrMsg] = useState(false);
 
   useEffect(() => {
     axios.get(`http://localhost:3001/notes`)
@@ -21,16 +22,25 @@ function Home() {
     setModal(!modal)
   }
 
-  const handleSave = () => [
+  const handleSave = () => {
+
+    // Does not save note without a title
+    if (newTitle== ''){
+      setTitleErrMsg(true)
+      return
+    }
     axios.post(`http://localhost:3001/notes`, {title: newTitle, postBody: ''})
       .then((response) => {
         setNewTitle('');
+        setTitleErrMsg(false)
         setModal(false);
         setListOfNotes([...listOfNotes, response.data]);
       })
-  ]
+  }
 
   return (
+    <>
+    <button onClick={handleClick} className='createBtn'>Create</button> 
     <div className="homeContainer">
         {listOfNotes.map((note, index) => {
             return (
@@ -40,24 +50,28 @@ function Home() {
             </div>
             );
         })}
-        <button onClick={handleClick} className='createBtn'>Create</button> 
-          { modal &&
-            <div className='modal'>
-              <div className='overlay'></div>
-              <div className='modalContent'>
-                <div className='modalTitle'>Create New Note</div>
-                <input 
-                  placeholder='Title Name' 
-                  value={newTitle} 
-                  className='titleInput'
-                  onChange = {(e)=> setNewTitle(e.target.value)}>
-                </input>
-                <button onClick={handleSave} className='save'>Save</button>
-                <button onClick={handleClick} className='closeModalButton'>Close</button>
-              </div>
-            </div>
-          }  
     </div>
+
+      { modal &&
+        <div className='modal'>
+          <div className='overlay'></div>
+            <div className='modalContent'>
+              <div className='modalTitle'>Create New Note</div>
+              <input 
+                placeholder='Title Name' 
+                value={newTitle} 
+                className='titleInput'
+                onChange = {(e)=> setNewTitle(e.target.value)}>
+              </input>
+            <button onClick={handleSave} className='save'>Save</button>
+            <button onClick={handleClick} className='closeModalButton'>Close</button>
+            { titleErrMsg &&
+              <div className='titleErrMessage'>Must enter a title</div>
+            }
+          </div>
+        </div>
+      }
+    </>      
   );
 };
 
