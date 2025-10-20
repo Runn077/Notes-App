8b237bb60467947924478fd166c91fae737d72a2
Runn077 <rupu0777@gmail.com>
2025-10-20 11:10:45 -0600

    created an endpoint to get a note by id

--- backend/routes/Notes.js
+++ backend/routes/Notes.js
@@ -2,11 +2,18 @@ const express = require("express");
 const router = express.Router();
 const { Notes } = require("../models")
 
+// get
 router.get("/", async (req, res) => {
     const listOfNotes = await Notes.findAll();
     res.json(listOfNotes);
 });
 
+router.get("/:id", async (req, res) =>{
+    const note = await Notes.findByPk(req.params.id)
+    res.json(note);
+})
+
+// post
 router.post("/", async (req, res) => {
     const note = req.body;
     await Notes.create(note);
