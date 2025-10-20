9117e02a4701058b33396e11d604ab85132698da
Runn077 <rupu0777@gmail.com>
2025-10-19 22:40:06 -0600

    added pop up for when creating a new note

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,8 +1,9 @@
-import React, {useEffect, useState}  from 'react';
+import React, {use, useEffect, useState}  from 'react';
 import axios from 'axios';
 
 function Home() {
   const [listOfNotes, setListOfNotes] = useState([]);
+  const [modal, setModal] = useState(false);
 
   useEffect(() => {
     axios.get(`http://localhost:3001/notes`)
@@ -15,16 +16,30 @@ function Home() {
     });
   }, []);
 
+  const handleClick = () =>{
+    setModal(!modal)
+  }
+
   return (
     <div className="homeContainer">
         {listOfNotes.map((note, index) => {
             return (
-            <div className = "note">
+            <div className = "note" key={index}>
                 <div className='title'>{note.title}</div>
                 <div className='postBody'>{note.postBody}</div>
             </div>
             );
-        })}  
+        })}
+        <button onClick={handleClick} className='createBtn'>Create</button> 
+          { modal &&
+            <div className='modal'>
+              <div className='overlay'></div>
+              <div className='modalContent'>
+                <div className='modalTitle'>Create New Note</div>
+                <button onClick={handleClick} className='closeModalButton'>Close</button>
+              </div>
+            </div>
+          }  
     </div>
   );
 };
