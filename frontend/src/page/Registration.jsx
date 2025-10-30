210ec49ea1f72b7313a07a95bf4124e25315f0ee
Runn077 <rupu0777@gmail.com>
2025-10-29 20:36:47 -0600

    changed structure of registration page

--- frontend/src/page/Registration.jsx
+++ frontend/src/page/Registration.jsx
@@ -3,10 +3,14 @@ import React from 'react'
 function Registration() {
   return (
     <div className='registeration container'>
-        <div className='registrationTitle'>Username</div>
-        <input className='username' placeholder='Enter Username'></input>
-        <div className='passwordTitle'>Password</div>
-        <input className='password' placeholder='Enter Password'></input>
+        <div className='usernameContainer'>
+            <p className='usernameTitle'>Username</p>
+            <input className='username' placeholder='Enter Username'></input>
+        </div>
+        <div className='passwordContainer'>
+            <p className='passwordTitle'>Password</p>
+            <input className='password' placeholder='Enter Password'></input>
+        </div>
         <button className='saveBtn'>Save</button>
     </div>
   )
