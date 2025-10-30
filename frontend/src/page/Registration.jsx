014dd199fb3000974e438ac50742935933614fd2
Runn077 <rupu0777@gmail.com>
2025-10-30 12:23:26 -0600

    Made inputs functional

--- frontend/src/page/Registration.jsx
+++ frontend/src/page/Registration.jsx
@@ -1,17 +1,55 @@
-import React from 'react'
+import { useState } from 'react'
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom';
+
 
 function Registration() {
+  const navigate = useNavigate();
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("")
+
+  const usernameChange = (e) => {
+    setUsername(e.target.value)
+  }
+  const passwordChange = (e) => {
+    setPassword(e.target.value)
+  }
+
+  const userSave = () =>{
+    axios.post(`http://localhost:3001/users/`, {username: username, password: password})
+      .then((response) => {
+        navigate(`/`)
+      })
+  }
+
   return (
     <div className='registeration container'>
         <div className='usernameContainer'>
             <p className='usernameTitle'>Username</p>
-            <input className='username' placeholder='Enter Username'></input>
+            <input 
+              className='username' 
+              placeholder='Enter Username'
+              value = {username}
+              onChange={usernameChange}
+            />
+
         </div>
         <div className='passwordContainer'>
             <p className='passwordTitle'>Password</p>
-            <input className='password' placeholder='Enter Password'></input>
+            <input 
+              className='password' 
+              placeholder='Enter Password'
+              value={password}
+              onChange={passwordChange}
+            />
         </div>
-        <button className='saveBtn'>Save</button>
+        <button 
+          className='saveBtn'
+          onClick={userSave}
+        >
+          Save
+        </button>
     </div>
   )
 }
