7200eb5e37ef7a5fa98af08ef4b525174eb6a815
Runn077 <rupu0777@gmail.com>
2025-10-30 22:04:15 -0600

    added styling to navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,11 +1,12 @@
 import { Link } from 'react-router-dom'
+import '../styles/navbar.css'
 
 function Navbar() {
     return (
         <nav className='navbar'>
             <div className='navbar-container'>
                 <div className='navbar-left'>
-                    <Link to='/' className='brand'>Notes</Link>
+                    <Link to='/' className='brand'>Really Cool Notes</Link>
                 </div>
                 <div className='navbar-right'>
                     <Link to='/' className='nav-link'>Home</Link>
