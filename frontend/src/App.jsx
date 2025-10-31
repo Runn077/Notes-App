3b86eab5eff096b18adc32b535c7550474294826
Runn077 <rupu0777@gmail.com>
2025-10-30 21:58:09 -0600

    added a navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,19 @@
 import './App.css'
-import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider, Outlet } from "react-router-dom"
 import Home from './page/Home'
 import Note from './page/Note'
 import Registration from './page/Registration'
+import Navbar from './components/Navbar'
 
-const router = createBrowserRouter([
-  {path:"/", element: <Home />},
-  {path:"/note/:id", element: <Note />},
-  {path:"/registration", element: <Registration />}
-])
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route element={<><Navbar /><Outlet /></>}>
+      <Route path="/" element={<Home />} />
+      <Route path="/note/:id" element={<Note />} />
+      <Route path="/registration" element={<Registration />} />
+    </Route>
+  )
+)
 
 function App() {
   return (
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import { Link } from 'react-router-dom'
+
+function Navbar() {
+    return (
+        <nav className='navbar'>
+            <div className='navbar-container'>
+                <div className='navbar-left'>
+                    <Link to='/' className='brand'>Notes</Link>
+                </div>
+                <div className='navbar-right'>
+                    <Link to='/' className='nav-link'>Home</Link>
+                    <Link to='/registration' className='nav-link'>Register</Link>
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
