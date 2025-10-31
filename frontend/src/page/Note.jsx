a7e913be6f1f815eac0056db124bf4598d2cf209
Runn077 <rupu0777@gmail.com>
2025-10-31 00:35:54 -0600

    Made text area change height

--- frontend/src/page/Note.jsx
+++ frontend/src/page/Note.jsx
@@ -1,10 +1,18 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef, useLayoutEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import axios from 'axios';
 
 function Note() {
   let {id} = useParams()  
   const [note, setNote] = useState({});
+  const titleRef = useRef(null);
+  const bodyRef = useRef(null);
+
+  const adjustHeight = (el) => {
+    if (!el) return;
+    el.style.height = 'auto';
+    el.style.height = `${el.scrollHeight}px`;
+  };
 
   useEffect(() => {
     axios.get(`http://localhost:3001/notes/${id}`)
@@ -13,8 +21,14 @@ function Note() {
         })
   }, [id]);
 
+  useLayoutEffect(() => {
+    adjustHeight(titleRef.current);
+    adjustHeight(bodyRef.current);
+  }, [note.title, note.postBody]);
+
   const handleChangeBody = async (e) => {
     const updatedText = e.target.value
+    adjustHeight(e.target);
     setNote({...note, postBody: updatedText})
     await axios.put(`http://localhost:3001/notes/${id}`, {
         ...note,
@@ -23,6 +37,7 @@ function Note() {
   }
   const handleChangeTitle = async (e) => {
     const updatedText = e.target.value
+    adjustHeight(e.target);
     setNote({...note, title: updatedText})
     await axios.put(`http://localhost:3001/notes/${id}`, {
         ...note,
@@ -34,12 +49,14 @@ function Note() {
     <div className='noteContainer'>
         <textarea 
             className='noteTitle'
+            ref={titleRef}
             value={note.title}
             onChange={handleChangeTitle}
             placeholder='Write Something!!!'
         />
         <textarea 
             className='noteText'
+            ref={bodyRef}
             value={note.postBody}
             onChange={handleChangeBody}
             placeholder='Write Something!!!'
