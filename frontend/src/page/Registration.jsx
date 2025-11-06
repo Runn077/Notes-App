f53e1fe53335ff79991f30208202a3f316160059
Runn077 <rupu0777@gmail.com>
2025-11-06 01:41:08 -0600

    added react hook form

--- frontend/src/page/Registration.jsx
+++ frontend/src/page/Registration.jsx
@@ -1,55 +1,17 @@
 import { useState } from 'react'
-import axios from 'axios'
-import { useNavigate } from 'react-router-dom';
+import { useForm } from 'react-hook-form'
 
 function Registration() {
-  const navigate = useNavigate();
-
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("")
-
-  const usernameChange = (e) => {
-    setUsername(e.target.value)
-  }
-  const passwordChange = (e) => {
-    setPassword(e.target.value)
-  }
-
-  const userSave = () =>{
-    axios.post(`http://localhost:3001/users/`, {username: username, password: password})
-      .then((response) => {
-        navigate(`/`)
-      })
+  const onSubmit = (data) => {
+    console.log(data)
   }
 
   return (
     <div className='registrationContainer'>
-        <div className='registrationCard'>
-          <div className='usernameContainer'>
-              <p className='usernameTitle'>Username</p>
-              <input 
-                className='username' 
-                placeholder='Enter Username'
-                value = {username}
-                onChange={usernameChange}
-              />
-          </div>
-          <div className='passwordContainer'>
-              <p className='passwordTitle'>Password</p>
-              <input 
-                className='password' 
-                placeholder='Enter Password'
-                value={password}
-                onChange={passwordChange}
-              />
-          </div>
-          <button 
-            className='saveBtn'
-            onClick={userSave}
-          >
-            Save
-          </button>
-        </div>
+        <form className='registrationCard' onSubmit={onSubmit}>
+        
+
+        </form>
     </div>
   )
 }
