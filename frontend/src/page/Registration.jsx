93a352b665f91d2cda3d07d3604944ea0c6965ee
Runn077 <rupu0777@gmail.com>
2025-11-06 12:43:14 -0600

    added form validation

--- frontend/src/page/Registration.jsx
+++ frontend/src/page/Registration.jsx
@@ -1,16 +1,54 @@
 import { useState } from 'react'
 import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+
+const schema = z.object({
+  username: z.string()
+    .nonempty("Username must be not empty")
+    .max(15, "Username must be less than 15 characters"),
+  password: z.string()
+    .nonempty("Password must be not empty")
+    .min(5, "Must be at least 5 characters"),
+  confirmPassword: z.string()
+}).refine((data) => data.password === data.confirmPassword, {
+  message: "Passwords do not match",
+  path: ["confirmPassword"], 
+});
 
 function Registration() {
+  const { register, handleSubmit, formState: {errors} } = useForm({
+    resolver: zodResolver(schema),
+  })
+
   const onSubmit = (data) => {
     console.log(data)
   }
 
   return (
     <div className='registrationContainer'>
-        <form className='registrationCard' onSubmit={onSubmit}>
-        
-
+        <form className='registrationCard' onSubmit={handleSubmit(onSubmit)}>
+          <input className='usernameInput' 
+          placeholder='Enter Username'
+          {...register("username")}
+          />
+          <p className='errorMessage'>{errors.username?.message}</p>
+          <input className='passwordInput' 
+          type='password' 
+          placeholder='Enter Password'
+          {...register("password")}
+          />
+          <p className='errorMessage'>{errors.password?.message}</p>
+          <input className='confirmPasswordInput' 
+          type='password' 
+          placeholder='Confirm Password'
+          {...register("confirmPassword")}
+          />
+          <p className='errorMessage'>{errors.confirmPassword?.message}</p>
+          <button className='saveBtn' 
+          type='submit'>
+            Submit
+          </button>
         </form>
     </div>
   )
