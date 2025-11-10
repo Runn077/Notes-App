ee4b61066619be3cf596befb070418fa068b3e27
Runn077 <rupu0777@gmail.com>
2025-11-09 22:19:50 -0600

    added bcrypt and created the login router

--- backend/routes/Auth.js
+++ backend/routes/Auth.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const router = express.Router();
-const { Users } = require("../models")
-const jwt = require("jsonwebtoken")
+const { Users } = require("../models");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 
 // post
 router.post("/register", async (req, res) => {
     try {
         const {username, password} = req.body;
+        const hashedPassword = await bcrypt.hash(password, 10);
 
         if (!username || !password) {
             return res.status(400).json({ error: "Username and password are required." });
@@ -16,7 +18,7 @@ router.post("/register", async (req, res) => {
             return res.status(400).json({ error: "Username already exists." });
         }
 
-        const newUser = await Users.create({ username, password });
+        const newUser = await Users.create({ username, password: hashedPassword });
         res.status(201).json({ message: "User registered successfully", user: newUser });
         
     } catch (err) {
@@ -26,8 +28,34 @@ router.post("/register", async (req, res) => {
 });
 
 router.post("/login", async (req, res) => {
-    const {username, password} = req.body
-    const user = await Users.findOne({ where: { username} });
+    try {
+        const {username, password} = req.body
+        const user = await Users.findOne({ where: {username} });
+
+        if (!user){
+            return res.status(400).json({error: "Username or password is incorrect"})
+        }
+
+        const validatePassword = await bcrypt.compare(password, user.password)
+        if (!validatePassword) {
+            return res.status(400).json({ error: "Password is incorrect" });
+        }
+        
+        const accessToken = jwt.sign({
+            id: user.id, 
+            username: user.username,
+        }, "SeceretKey");
+        
+        res.json({
+            id: user.id, 
+            username: user.username,
+            accessToken,
+        })
+
+    } catch (error){
+        console.error(error)
+
+    }    
 })
 
 // router.get("/me")
